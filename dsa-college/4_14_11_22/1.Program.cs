29fc5e6eb25e7529e3ee508eb4deec72ffb96d97
sagivalia11@gmail.com <sagivalia11@gmail.com>
2022-11-14 21:20:59 +0200

    edited

---
+++ dsa-college/4_14_11_22/1.Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace dsa_college._4_14_11_22
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
