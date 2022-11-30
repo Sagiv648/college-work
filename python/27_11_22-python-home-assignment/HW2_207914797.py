faefb45477a29080c1daf6230c03b03e87561cf8
sagivalia11@gmail.com <sagivalia11@gmail.com>
2022-11-30 20:18:15 +0200

    edited

--- python/27_11_22-python-home-assignment/HW2_207914797.py
+++ python/27_11_22-python-home-assignment/HW2_207914797.py
@@ -18,7 +18,12 @@ def Q1(dic):
     return False
 
 def Q2(seq):
-    pass
+    '''
+    The procedure filters the list by the items which their length as a string equal to their length as a set
+    if they are equal the number doesn't have duplicate digits
+    else it does
+    '''
+    return list(filter(lambda x: len(str(x)) == len(reduce(lambda a,b: a.union(b) ,str(x), set())) ,seq))
 
 def Q3(dic):
     '''
